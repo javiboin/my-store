9be93110a9823302669a3a59edff222661f7b95a
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-09-20 21:55:50 -0300

    Add: more routes

--- index.js
+++ index.js
@@ -7,11 +7,71 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+})
+
+app.get('/users', (req, res) => {
+  res.json([
+    {
+    id: 1,
+    name: 'John Doe'
+    },
+    {
+      id: 2,
+      name: 'Cachilo'
+    }
+  ]);
+});
+
 app.get('/products', (req, res) => {
+  res.json([
+    {
+    name: 'Producto 1',
+    price: '$100'
+    },
+    {
+      name: 'Producto 2',
+      price: '$100'
+    }
+  ]);
+});
+
+app.get('/categories', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Categoria 1'
+    },
+    {
+      id: 2,
+      name: 'Categoria 2'
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
+    id,
     name: 'Producto 1',
     price: '$100'
   });
+});
+
+/* app.get('/users', (req, res) =>{
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.
+  }
+}) */
+
+app.get('/categories/:categoryId/products/:productsId', (req, res) => {
+  const { categoryId, productsId } = req.params;
+  res.json({
+    categoryId: categoryId,
+    productId: productsId
+  })
 })
 
 app.listen(port, () => {
