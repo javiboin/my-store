e35aaa38eb8301d2a955d592d070d369c02f1329
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-09-25 22:42:18 -0300

    Update: Async await on Products

--- routes/products.route.js
+++ routes/products.route.js
@@ -2,48 +2,83 @@ const express = require('express');
 const ProductService = require('../services/products.service');
 
 const router = express.Router();
-
 const service = new ProductService();
 
-router.get('/', (req, res) => {
-  const products = service.find();
-  res.status(200).json(products);
+router.get('/', async (req, res) => {
+  try {
+    const products = await service.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
 router.get('/filter', (req, res) => {
   res.status(200).send('Yo soy un filter');
 })
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const product = service.findOne(id);
-  res.status(200).json(product);
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  const newProduct = service.create(body);
-  res.status(201).json(newProduct);
+router.post('/', async (req, res) => {
+  try {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 })
 
-router.put('/', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const product = service.update(id, body);
-  res.status(200).json(product)
+router.put('/', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 })
 
-router.patch('/', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const product = service.update(id, body);
-  res.status(200).json(product)
+router.patch('/', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 })
 
-router.delete('/', (req, res) => {
-  const { id } = req.params;
-  const product = service.delete(id);
-  res.status(200).json(product)
+router.delete('/', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await service.delete(id);
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 })
 
 module.exports = router;
--- services/products.service.js
+++ services/products.service.js
@@ -19,7 +19,7 @@ class ProductService {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
@@ -28,15 +28,19 @@ class ProductService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 3000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find(item => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
       throw new Error('product not found');
@@ -49,7 +53,7 @@ class ProductService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
       throw new Error('product not found');
