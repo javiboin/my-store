338231bc7de55e59fc28e1c50c5a9f6bceac6d75
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-09-26 00:13:16 -0300

    Add: Error handler on products

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const routerApi = require('./routes');
-// const { faker } = require('@faker-js/faker');
-const app = express();
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler')
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -12,6 +13,9 @@ app.get('/', (req, res) => {
 })
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('mi port ' + port);
--- routes/products.route.js
+++ routes/products.route.js
@@ -19,15 +19,13 @@ router.get('/filter', (req, res) => {
   res.status(200).send('Yo soy un filter');
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
     const product = await service.findOne(id);
     res.status(200).json(product);
   } catch (error) {
-    res.status(404).json({
-      message: error.message
-    });
+    next(error);
   }
 });
 
