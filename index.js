27251e52ba16656d9a4b3e8d814a033468e4c546
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-09-28 21:31:44 -0300

    Update: deploy with heroku

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-const whitelist = ['http://localhost:8080', 'https://myapp.com'];
+const whitelist = ['http://localhost:8080', 'https://calm-hollows-94680.herokuapp.com/'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
--- routes/categories.route.js
+++ routes/categories.route.js
@@ -1,48 +1,42 @@
 const express = require('express');
 const CategoryService = require('../services/categories.service');
-const validatorHandler = require('../services/categories.service');
+const validatorHandler = require('../middlewares/validator.handler');
 const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('../schemas/categories.schema');
 
 const router = express.Router();
 const service = new CategoryService();
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const categories = await service.find();
     res.status(200).json(categories);
   } catch (error) {
-    res.status(404).json({
-      message: error.message
-    })
+    next(error);
   }
 });
 
 router.get('/:id',
   validatorHandler(getCategorySchema, 'params'),
-  async (req, res) => {
+  async (req, res, next) => {
     try {
       const { id } = req.params;
       const categories = await service.findOne(id);
       res.status(200).json(categories);
     } catch (error) {
-      res.status(404).json({
-        message: error.message
-      })
+      next(error);
     }
   }
 )
 
 router.post('/',
   validatorHandler(createCategorySchema, 'body'),
-  async (req, res) => {
+  async (req, res, next) => {
     try {
       const body = req.body;
       const newCategory = await service.create(body);
       res.status(201).json(newCategory)
     } catch (error) {
-      res.status(404).json({
-        message: error.message
-      })
+      next(error);
     }
   }
 )
@@ -50,16 +44,14 @@ router.post('/',
 router.put('/',
   validatorHandler(getCategorySchema, 'params'),
   validatorHandler(updateCategorySchema, 'body'),
-  async (req, res) => {
+  async (req, res, next) => {
     try {
       const { id } = req.params;
       const body = req.body;
       const category = await service.update(id, body);
       res.status(200).json(category)
     } catch (error) {
-      res.status(404).json({
-        message: error.message
-      })
+      next(error);
     }
   }
 )
@@ -67,31 +59,27 @@ router.put('/',
 router.patch('/',
   validatorHandler(getCategorySchema, 'params'),
   validatorHandler(updateCategorySchema, 'body'),
-  async (req, res) => {
+  async (req, res, next) => {
     try {
       const { id } = req.params;
       const body = req.body;
       const category = await service.update(id, body);
       res.status(200).json(category)
     } catch (error) {
-      res.status(404).json({
-        message: error.message
-      })
+      next(error);
     }
   }
 )
 
 router.delete('/',
   validatorHandler(getCategorySchema, 'params'),
-  async (req, res) => {
+  async (req, res, next) => {
     try {
       const { id } = req.params;
       const category = await service.delete(id);
       res.status(200).json(category)
     } catch (error) {
-      res.status(404).json({
-        message: error.message
-      })
+      next(error);
     }
   }
 )
