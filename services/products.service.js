d2607d97784a2ee86114d5abd4cf581c3494df14
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2022-10-13 20:28:32 -0300

    Add connection pool in products

--- services/products.service.js
+++ services/products.service.js
@@ -1,11 +1,15 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
+const pool = require('../libs/postgres.pool')
+
 class ProductService {
 
   constructor() {
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
   }
 
   generate() {
@@ -31,11 +35,14 @@ class ProductService {
   }
 
   async find() {
-    return new Promise((resolve) => {
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
+    return rta.rows;
+    /* return new Promise((resolve) => {
       setTimeout(() => {
         resolve(this.products);
       }, 3000);
-    });
+    }); */
   }
 
   async findOne(id) {
